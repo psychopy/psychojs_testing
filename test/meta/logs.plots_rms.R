0d65ec86c330ce70172bf1b5a8767c19b6617b77
unknown <pronkthomas@gmail.com>
2020-10-22 16:29:28 +0100

    Dropped R-scripts

--- test/meta/logs.plots_rms.R
+++
@@ -1,5 +0,0 @@
-library(ggplot2)
-ds <- read.table('../.tmp/processed_logs/rms_values.csv', header = TRUE, sep = '\t')
-ggplot(ds, aes(rms)) +
-  geom_histogram() + 
-  facet_grid(rows = vars(test))
