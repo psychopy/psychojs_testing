9c564b7dc2142c6213e422a99db5a831115ea41f
unknown <pronkthomas@gmail.com>
2020-11-03 14:04:21 +0000

    more robust

--- test/cli/deleteLogs.js
+++ test/cli/deleteLogs.js
@@ -1,22 +1,35 @@
 // Delete reports on staging server and browserstack logs
-var child_process = require('child_process');
 
-// Parse CLI options, if any
+// *** Parse CLI arguments
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
 
-// Construct CLI options for updateStager.js and updateBrowserStack.js
-let updateBrowserStackArgs = [];
-let updateStagerArgs = ['--action', 'delete'];
-// CLI option: branch
-if (argv.branch !== undefined) {
-  console.log('deleteLogs.js: branch CLI option specified as ' + argv.branch);
-  updateBrowserStackArgs = updateBrowserStackArgs.concat(['--build', argv.branch]);  
-  updateStagerArgs = updateStagerArgs.concat(['--branch', argv.branch])
-} 
+// *** Get branch name from TRAVIS_BRANCH or CLI argument
+let branch;
+if (process.env.TRAVIS_BRANCH !== undefined) {
+  console.log('deleteLogs.js: branch specified via TRAVIS_BRANCH as ' + process.env.TRAVIS_BRANCH);
+  branch = process.env.TRAVIS_BRANCH;
+} else if (argv.branch !== undefined) {
+  console.log('deleteLogs.js: branch specified via CLI option as ' + argv.branch);
+  branch = argv.branch;
+} else {
+  throw "deleteLogs.js: No branch specified via TRAVIS_BRANCH or CLI option";
+}
 
-// Run updateBrowserStack.js
-child_process.fork('test/cli/updateBrowserStack.js', updateBrowserStackArgs);
-// Run updateStager.js
-child_process.fork('test/cli/updateStager.js', updateStagerArgs);
+// Delete branch in BrowserStack logs if required environment vars are available
+if (process.env.BROWSERSTACK_USER === undefined || process.env.BROWSERSTACK_ACCESSKEY === undefined) {
+  console.log('deleteLogs.js: could not find BROWSERSTACK_USER or BROWSERSTACK_ACCESSKEY. Skipping deletion of BrowserStack logs');
+} else {
+  const BrowserStack = require('../shared/BrowserStack.js');
+  BrowserStack.deleteOneBuild(branch);
+}
+
+// Delete branch on staging server if required environment vars are available
+if (process.env.STAGING_PORT === undefined ||  process.env.STAGING_USERNAME === undefined || process.env.STAGING_PASSWORD === undefined) {
+  console.log('deleteLogs.js: could not find STAGING_PORT, STAGING_USERNAME, or STAGING_PASSWORD. Skipping deletion of reports on staging server');
+} else {
+  const Stager = require('../shared/Stager.js');
+  Stager.deleteDirectory(branch);
+}
+
