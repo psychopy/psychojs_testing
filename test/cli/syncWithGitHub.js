1e0d1847014725b2a1096affd34af410d982ccdb
unknown <pronkthomas@gmail.com>
2020-11-18 07:44:25 +0000

    Don't delete pseudo-branches specified via CLI

--- test/cli/syncWithGitHub.js
+++ test/cli/syncWithGitHub.js
@@ -1,14 +1,22 @@
 // Synchronize BrowserStack and Stager logs with GitHub
 // i.e. delete any logs that don't have matching branches in GitHub
+// Any unnamed CLI options are 'pseudo-branches' whose logs will be kept, regardless of whether they exist on GitHub
 
 // *** Modules
 const GitHub = require('../shared/GitHub.js');
 const BrowserStack = require('../shared/BrowserStack.js');
 const Stager = require('../shared/Stager.js');
 
+// Parse CLI arguments
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+const pseudoBranches = argv._;
+
 // *** Perform action
 console.log('syncWithGithub.js: retrieving GitHub branches');
-const branchNames = GitHub.getBranchNames();
+let branchNames = GitHub.getBranchNames().concat(pseudoBranches);
+console.log(branchNames);
 console.log('syncWithGithub.js: deleting BrowserStack builds');
 BrowserStack.deleteAllBranchesExcept(branchNames);
 console.log('syncWithGithub.js: deleting staging server reports');
