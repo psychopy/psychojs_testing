69cd0258571c97711fb3e08f8c7613e73c9a1d42
unknown <pronkthomas@gmail.com>
2020-12-02 01:49:28 +0000

    demo unit tests

--- test/specs_unit/demo_test.js
+++ test/specs_unit/demo_test.js
@@ -1,7 +1,8 @@
 import * as util from './../../dist/util-2020.2.js';
-describe('util', () => {
+describe('demo_test', () => {
   it('isInt', () => {
     expect(util.isInt(5)).toEqual(true);
+    expect(util.isInt('xx')).toEqual(true);
     expect(util.isInt(1.1)).toEqual(false);
     expect(util.isInt(0)).toEqual(true);
     expect(util.isInt('aa')).toEqual(true);
