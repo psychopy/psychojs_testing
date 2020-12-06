4601bde0ecf8e8a09f4146b2dfff395649d1e9ff
unknown <pronkthomas@gmail.com>
2020-12-06 17:12:39 +0000

    Update single_test.cjs

--- test/specs_e2e/single_test.cjs
+++ test/specs_e2e/single_test.cjs
@@ -2,11 +2,11 @@ const SharedBehaviors = require('../shared/SharedBehaviors.cjs');
 describe('single_test', () => {
   let test, testFunction;
   it('get_test', () => {
-    console.log('[single_test.cjs] get test')
+    console.log('[single_test.cjs] get_test')
     test = browser.getTest();
     testFunction = require('../tests_e2e/' + test + '.cjs').run;
   });
-  it(test, () => {
+  it('run_test', () => {
     console.log('[single_test.cjs] ' + test)
     testFunction();
   });
