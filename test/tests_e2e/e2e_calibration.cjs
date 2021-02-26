9098a0952a9a9cee2ced36f9c227953466ccbeb0
Thomas Pronk <thomas.pronk@nottingham.ac.uk>
2021-02-26 11:10:33 +0000

    TEST+RF: Remove tests that have been relocated

--- test/tests_e2e/e2e_calibration.cjs
+++
@@ -1,7 +0,0 @@
-const SharedBehaviors = require('../shared/SharedBehaviors.cjs');
-
-module.exports = {
-  run: (calibration = null) => {
-    SharedBehaviors.performCalibrationExperiment(true);
-  }
-};
