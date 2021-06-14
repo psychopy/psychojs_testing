ad1b4a8460b242050314f595c225d8ebfabad649
Thomas Pronk <thomas.pronk@nottingham.ac.uk>
2021-06-14 13:53:21 +0100

    ENH: Test callbacks that run once and multiple times

--- tests/wdio_button/wdio_button.cjs
+++ tests/wdio_button/wdio_button.cjs
@@ -14,15 +14,23 @@ module.exports = {
     SharedBehaviors.waitForReport('intro');
     SharedBehaviors.tapAtCoordinate(calibration.transformX(0), calibration.transformY(0));
 
-    // *** button
-    // Click button, check data-report (and later data-output)
-    SharedBehaviors.waitForReport('button');
+    // *** button1_trial
+    // Make screenshot, click button, check data-output
+    SharedBehaviors.waitForReport('button1_trial');
+    browser.writeScreenshot('button1_trial');
     SharedBehaviors.tapAtCoordinate(calibration.transformX(0), calibration.transformY(0.25));
-
-    // Wait until experiment finished
-    SharedBehaviors.waitForReport('FINISHED');
     browser.waitUntil(() => {
       return $('<body>').getAttribute('data-output') === 'callback';
     }, {timeoutMsg: 'Attribute data-output of body in HTML was not callback; maybe the button callback was not called?'});
+
+    // *** button2_trial
+    // Click button (for a second), check data-output
+    SharedBehaviors.waitForReport('button2_trial');
+    SharedBehaviors.tapAtCoordinate(calibration.transformX(0), calibration.transformY(-0.25), 1000);
+    const numberOfFrames = Number($('<body>').getAttribute('data-output'));
+    expect(numberOfFrames).withContext('Checking if thecallback of button2 was called more than once').toBeGreaterThan(1);
+    // Finish routine and wait until experiment finished
+    SharedBehaviors.tapAtCoordinate(calibration.transformX(0), calibration.transformY(0));
+    SharedBehaviors.waitForReport('FINISHED');
   }
 };
